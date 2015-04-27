73c621dac49661830040828f3fcff4afda3b0a59
Paul Barrett <unknown.unit@gmail.com>
2015-04-27 17:16:17 +0200

    REPL namespace utilities
    - util for reloading all code from watched namespaces
      - uses ns-tracker
      - this will likely be replaced with tools.namespace
    - util for taking `reload-ns`, `ns+`, `ns-) to other namespaces
      for easier access in any namespace
      - also supports additional namespaces to follow along,
        with rules for visibility

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject unknown_unit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+(defproject clojure-utils "0.1.0-SNAPSHOT"
+  :description "A set of miscellaneous utilities for Clojure development"
+  :url "https://bitbucket.org/unknown-unit/clojure-utilities"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ns-tracker "0.2.2"]])
---
+++ src/unknown_unit/repl/ns.clj
@@ -0,0 +1,67 @@
+(ns unknown-unit.repl.ns
+  (:require [ns-tracker.core :refer :all]))
+
+;; Define this if you want your core functionality namespaces instead of locally referred.
+;; Requires a symbol, e.g., 'core
+(def ^:private ns-alias nil)
+
+;; If you want to alias the main namespace to something,
+;; modify like this (assuming my-util is the desired name):
+;; [~(symbol (str *ns*)) :as 'my-util]
+(def ^:private core-namespace
+  (let [ns-symbol [(symbol (str *ns*))]]
+    (if ns-alias
+      (conj ns-symbol :as ns-alias)
+      ns-symbol)))
+
+;; Always includes the current namespace, so that
+;; ns+, ns- and reload-ns are always available.
+;; Add namespaces you want included, e.g.,
+;; [:clojure.set :as set].
+(def ^:private user-namespaces
+  '[])
+
+(def ^:private follow-namespaces
+  (cons core-namespace user-namespaces))
+
+(def ^:private modified-namespaces
+  (ns-tracker ["src" "test"]))
+
+;; Namespace reloading tools.
+
+(declare refer-some)
+
+(defn reload-ns
+  []
+  (doseq [ns-sym (modified-namespaces)]
+    (require ns-sym :reload))
+  (require (refer-some core-namespace) :reload))
+
+;; Namespace traversal tools.
+
+(defn- refer-all
+  [namespace]
+  [(first namespace) :refer :all])
+
+(defn- refer-some
+  [namespace]
+  (if (some #{:as :refer} namespace)
+    namespace
+    (refer-all namespace)))
+
+(defmacro follow-ns
+  [namespace & {:keys [refer-all?]}]
+  (let [ref-fn (if refer-all? refer-all refer-some)
+        follow-namespaces (map ref-fn follow-namespaces)]
+    `(do
+       (reload-ns)
+       (ns ~namespace
+         (:require ~@follow-namespaces)))))
+
+(defmacro ns+
+  [namespace]
+  `(follow-ns ~namespace :refer-all? true))
+
+(defmacro ns-
+  [namespace]
+  `(follow-ns ~namespace))
