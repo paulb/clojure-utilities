286055f86ac580557a91df64fb3c1779c85bc02c
Paul Barrett <unknown.unit@gmail.com>
2015-05-13 19:49:51 +0200

    This is not a commit

--- src/unknown_unit/repl/ns.clj
+++ src/unknown_unit/repl/ns.clj
@@ -8,9 +8,10 @@
 ;; Namespaces are defined in require format:
 ;; [unladen.swallow]
 ;; [unladen.swallow :as swallow]
-;; [unladen.swallow :refer [coconuts]]
-;; [unladen.swallow :as swallow :refer [coconuts]]
+;; [laden.swallow :refer [coconuts]]
+;; [laden.swallow :as swallow :refer [coconuts]]
 
+(def ^:private auto-refresh? (boolean (config/get :auto-refresh)))
 (def ^:private local-ns (symbol (str *ns*)))
 ;; Define this if you want your core functions aliased
 ;; instead of locally referred.
@@ -32,8 +33,6 @@
     (when-not (= ns-name local-ns) (require `[~ns-name]))
     (let [referrals (var-get (intern ns-name 'referrals))]
       (conj namespace :refer referrals))))
-
-(def mod-namespaces (ns-tracker ["src" "test"]))
 
 (defn- core-namespaces*
   []
@@ -71,7 +70,6 @@
   (into (core-namespaces) (user-namespaces)))
 
 (def ^:private traveling-namespaces (memoize traveling-namespaces*))
-
 (def ^:private modified-namespaces (ns-tracker (into ["src" "test"] (config/get :watch))))
 
 (defn- load-namespaces
@@ -79,11 +77,9 @@
   (doseq [namespace namespaces]
     (require namespace :reload)))
 
-(defn init
-  []
-  (load-namespaces (traveling-namespaces)))
-
 ;; NOTE Config reloading is only useful in a dev environment.
+;; TODO Don't do it unless in dev environment
+;;      (requires environment awareness, this can go in config)
 ;; TODO Add config-dir to config, which allows reloading config
 ;;      to add new user libraries at any time.
 (defn reload-ns
@@ -108,3 +104,102 @@
 (defmacro ns-
   [namespace]
   `(follow-ns ~namespace))
+
+;; Initialize/Start/Stop
+
+(declare start)
+
+(defn running
+  [system]
+  (assoc system :running true))
+
+(defn stopped
+  [system]
+  (assoc system :running false))
+
+(def ^:private system*
+  (atom {:running false
+         :start start
+         :stop [stopped]}))
+
+(defn system
+  []
+  @system*)
+
+(defn start
+  ([] (println :default-start) (start system*))
+  ([system]
+   (println :repl-util :tools :start
+            :with system)
+   (if-not (:running system)
+     (do
+       (println :auto-refresh-cfg (config/get :auto-refresh))
+       (println :auto-refresh auto-refresh?)
+       (load-namespaces (traveling-namespaces))
+       (if auto-refresh?
+         (let [refresh-interval (config/get :refresh-interval 500)]
+           (println :refreshing--)
+           (let [refresh-ms (config/get :refresh-ms)
+                 _ (println :and :then...)
+                 loader (future (while true
+                                  (Thread/sleep refresh-interval)
+                                  (reload-ns)))
+                 _ (println :and :after :that...)]
+             (-> (update-in system [:stop]
+                            conj `(future-cancel ~loader))
+                 running
+                 ((fn [x]
+                    (println)
+                    (println :current-system x)
+                    (println)
+                    x)))))
+         (running system)))
+     system)))
+
+(def ^:private initialized (atom false))
+
+(defn init
+  []
+  (when-not @initialized
+    (println :initialize :system)
+    (reset! initialized true)
+    (swap! system* start)
+    (println :siestem @system*)
+    (println :SYSTEM (system))))
+
+(defn stop
+  [system]
+  (println :stopping)
+  (doall (:stop system)))
+
+(defn reset
+  [system]
+  (stop system)
+  (start system))
+
+(defn -start
+  [this](println :this-one!!!!!!!)
+  (start))
+
+(defn -stop
+  [this]
+  (stop))
+
+(defn -destroy
+  [this]
+  ;; no-op
+  )
+
+(defn -main
+  [& args]
+  ; (.addShutdownHook (Runtime/getRuntime) (do (stop) (Thread. stop)))
+  (start))
+
+(def atom-test (atom {}))
+
+(defn moo-cow
+  [thing]
+  (assoc thing :moooooo! "hello cow!"))
+
+(def fuck (atom ()))
+
