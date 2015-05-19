e8e1ef8ca8998b952dc848bc268527c680524c22
Paul Barrett <unknown.unit@gmail.com>
2015-05-19 09:35:18 +0200

    Reinstate 'init once'

--- src/unknown_unit/repl/core.clj
+++ src/unknown_unit/repl/core.clj
@@ -3,8 +3,18 @@
             [unknown-unit.repl.system :as system])
   (:refer-clojure :exclude [ns]))
 
+(def ^:private initialized (atom false))
+
 (defn init
   []
-  (ns/init)
-  (system/configure :ns-refresh ns/reload-ns)
-  (system/start))
+  (when-not @initialized
+    (ns/init)
+    (system/configure :ns-refresh ns/reload-ns)
+    (system/start)
+    (reset! initialized true)))
+
+(defn re-init
+  []
+  (system/stop)
+  (reset! initialized false)
+  (init))
